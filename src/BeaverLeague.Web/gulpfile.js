2e15776b2c9f43afdc230149924c4dfa7b5238c9
K. Scott Allen <scott@OdeToCode.com>
2016-10-04 21:06:31 -0400

    setting up front-end build

---
+++ src/BeaverLeague.Web/gulpfile.js
@@ -0,0 +1,19 @@
+﻿var gulp = require("gulp");
+var del = require("del");
+
+gulp.task("clean:assets",
+    function() {
+        return del("wwwroot/assets");
+    });
+
+gulp.task("copy:assets", ["clean:assets"],
+    function() {
+        return gulp.src([
+                "node_modules/font-awesome/css/**/*",
+                "node_modules/font-awesome/fonts/**/*",
+                "node_modules/bootstrap/dist/**/*"
+            ], {base:"node_modules"})
+            .pipe(gulp.dest("wwwroot/assets"));
+    });
+
+gulp.task("default", ["copy:assets"]);
