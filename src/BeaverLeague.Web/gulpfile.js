f084c36a8af6e88707ff672355e4106497030a27
K. Scott Allen <scott@OdeToCode.com>
2016-10-05 11:16:36 -0400

    themes and css

--- src/BeaverLeague.Web/gulpfile.js
+++ src/BeaverLeague.Web/gulpfile.js
@@ -1,12 +1,22 @@
 ﻿var gulp = require("gulp");
 var del = require("del");
+var cleanCss = require("gulp-clean-css");
 
 gulp.task("clean:assets",
     function() {
         return del("wwwroot/assets");
     });
 
-gulp.task("copy:assets", ["clean:assets"],
+gulp.task("css:minify",
+    ["clean:assets"],
+    function() {
+        return gulp.src("wwwroot/app/css/theme.css")
+            .pipe(cleanCss())
+            .pipe(gulp.dest("wwwroot/assets/app/css"));
+    });
+
+gulp.task("copy:assets",
+    ["clean:assets"],
     function() {
         return gulp.src([
                 "node_modules/font-awesome/css/**/*",
@@ -16,4 +26,4 @@ gulp.task("copy:assets", ["clean:assets"],
             .pipe(gulp.dest("wwwroot/assets"));
     });
 
-gulp.task("default", ["copy:assets"]);
+gulp.task("default", ["copy:assets", "css:minify"]);
