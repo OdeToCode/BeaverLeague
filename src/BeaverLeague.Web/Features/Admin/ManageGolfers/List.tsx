078e837c1b702cb001e80e427cbf5ad86c710f1c
K. Scott Allen <scott@OdeToCode.com>
2016-11-27 19:20:17 -0500

    setup to build each tsx

---
+++ src/BeaverLeague.Web/Features/Admin/ManageGolfers/List.tsx
@@ -0,0 +1,4 @@
+﻿import * as React from "React";
+import {name} from "common/components/greeting";
+
+alert(name);
--- src/BeaverLeague.Web/webpack.config.js
+++ src/BeaverLeague.Web/webpack.config.js
@@ -1,24 +1,32 @@
 var webpack = require("webpack");
 var path = require("path");
-
 var assets = path.join(__dirname, "wwwroot", "assets");
+var glob = require("glob");
+
+var entries = {};
+var files = glob.sync("./Features/**/*.tsx");
+files.forEach(file => {
+    var name = file.match("./Features(.+/[^/]+)\.tsx$")[1];
+    entries[name] = file;
+});
 
 module.exports = {
     resolve: {
-        extensions: ["", ".ts", ".tsx", ".js"]
+        extensions: ["", ".ts", ".tsx", ".js"],
+        modulesDirectories: [
+            "./Client/script/"
+        ]
     },
-    entry: {
-        
-    },
+    entry: entries,
     output: {
         path: assets,
-        filename: "[name].js",      
+        filename: "[name].js"    
     },
     module: {
-    loaders: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
-    },
+        loaders: [
+          { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }, 
     plugins: [
          new webpack.DllReferencePlugin({            
              context: ".",
