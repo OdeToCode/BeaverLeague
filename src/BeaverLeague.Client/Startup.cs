d4ce278f856484e0dfdf32a5ecaa4a0c96bad1c8
Scott Allen <scott@odetocode.com>
2018-09-03 09:42:04 -0400

    trying to get a roster display

--- src/BeaverLeague.Client/Startup.cs
+++ src/BeaverLeague.Client/Startup.cs
@@ -1,11 +1,18 @@
 ﻿using BeaverLeague.Client.Components;
+using BeaverLeague.Client.Services;
+using Blazor.Extensions.Logging;
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddSingleton<ApiClient>();
+        services.AddLogging(builder => builder
+            .AddBrowserConsole()
+            .SetMinimumLevel(LogLevel.Trace));    
     }
 
     public void Configure(IBlazorApplicationBuilder app)
---
+++ src/BeaverLeague.Web/Api/GolfersController.cs
@@ -0,0 +1,20 @@
+﻿using BeaverLeague.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BeaverLeague.Web.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GolfersController : ControllerBase
+    {
+        public ActionResult<List<Golfer>> Get()
+        {
+            return new List<Golfer>()
+            {
+                new Golfer  { Id= 1, FirstName="Scott", LastName="Allen"},
+                new Golfer { Id = 2, FirstName="Blarg", LastName="BlargFace"}
+            };
+        }
+    }
+}
