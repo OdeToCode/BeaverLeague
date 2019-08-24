537598ea77c2411116993c8537d6b77be754f834
Scott Allen <scott@odetocode.com>
2019-08-24 06:01:53 -0400

    remove db project

--- src/BeaverLeague.DB/MatchSet.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[MatchSets]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[Date] datetime NOT NULL,
-	[SeasonID] INT NOT NULL,
-	CONSTRAINT FK_MatchSets_Seasons FOREIGN KEY (SeasonID) REFERENCES Seasons(Id)
-)
--- src/BeaverLeague.DB/Season.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Seasons]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[Name] varchar(80) NOT NULL
-)
