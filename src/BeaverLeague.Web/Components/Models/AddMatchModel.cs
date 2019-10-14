22b0eab57474936a9f33689a7c999951bf755209
OdeToCode <scott@OdeToCode.com>
2019-10-13 21:03:03 -0400

    putting together scoring form

---
+++ src/BeaverLeague.Web/Components/Models/AddMatchModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeaverLeague.Web.Components.Models
+{
+    public class AddMatchModel
+    {
+        public int GolferOneId { get; set; }
+        public int GolferOneScore { get; set; }
+        public decimal GolferOnePoints { get; set; }
+        public bool GolferOnePlayNextWeek { get; set; }
+
+        public int GolferTwoId { get; set; }
+        public int GolferTwoScore { get; set; }
+        public decimal GolferTwoPoints { get; set; }
+        public bool GolferTwoPlayNextWeek { get; set; }
+    }
+}
