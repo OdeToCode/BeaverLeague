7c6fb57ff7a98b517affaacb31ebbcb9c1649160
OdeToCode <scott@OdeToCode.com>
2019-10-16 12:31:00 -0400

    basic combo box features

--- src/BeaverLeague.Web/Components/WeeklyScores.cs
+++ src/BeaverLeague.Web/Components/WeeklyScores.cs
@@ -4,6 +4,8 @@ using BeaverLeague.Data.Services;
 using BeaverLeague.Web.Components.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BeaverLeague.Web.Components
@@ -12,6 +14,28 @@ namespace BeaverLeague.Web.Components
     {
         [Inject] public LeagueData leagueData { get; set; } = null!;
         [Inject] public IJSRuntime jsRuntime { get; set; } = null!;
+
+
+        public Golfer SelectedGolfer { get; set; } = null!;
+        public IList<Golfer> Golfers { get; set; } = new List<Golfer>()
+        {
+            new Golfer { FirstName = "Aname", LastName="Habano" },
+            new Golfer { FirstName = "Foo", LastName="Jesus" },
+            new Golfer { FirstName = "Boo", LastName="Wassle" },
+            new Golfer { FirstName = "Baz", LastName="Compton" },
+            new Golfer { FirstName = "Doo", LastName="Reznikov" },
+            new Golfer { FirstName = "Lou", LastName="Reidodp" },
+            new Golfer { FirstName = "Too", LastName="Fislsll" },
+            new Golfer { FirstName = "Moo", LastName="Gope" },
+            new Golfer { FirstName = "Dooey", LastName="Blazo" },
+            new Golfer { FirstName = "Mooey", LastName="Blargy" },
+            new Golfer { FirstName = "Spitooey", LastName="Argy" }
+        };
+
+        public Func<string, Func<Golfer, bool>> GolferFilter = s =>
+        {
+            return g => g.FirstName.Contains(s, StringComparison.CurrentCultureIgnoreCase) || g.LastName.Contains(s, StringComparison.CurrentCultureIgnoreCase);
+        };
 
         protected bool initialized = false;
         protected MatchSet? matchSet = null;
