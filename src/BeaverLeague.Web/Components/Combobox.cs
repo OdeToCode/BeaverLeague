da65b8c307edb0ad33f3dcd72f8c4ec5a3d6c92c
OdeToCode <scott@OdeToCode.com>
2019-10-15 21:17:07 -0400

    must have a blazor combobox

---
+++ src/BeaverLeague.Web/Components/Combobox.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BeaverLeague.Web.Components
+{
+    public class ComboboxBase<T> : ComponentBase, IDisposable
+    {
+        public void Dispose()
+        {
+            
+        }
+    }
+}
